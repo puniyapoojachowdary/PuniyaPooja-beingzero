78b87a1f07d8206264884ef8620469cfafb1b81f
puniyapoojachowdary <poojachowdary2601@gmail.com>
2019-12-19 16:15:41 +0530

    JSON

--- public/js/site.js
+++ public/js/site.js
@@ -1,11 +1,7 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-app.use(express.static('public'))
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
-var obj = {}
-obj.name = "Being Zero"
-obj.college = "cmrcet"
-obj.reg = "17h51a05g5"
-app.get('/data', (req, res) => res.json(obj))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+$(document).ready(function(){
+    $.getJSON("/data",function(d){
+        alert(JSON.stringify);
+        $("#name".text(d.name));
+    })
+})

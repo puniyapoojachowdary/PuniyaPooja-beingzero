3cf09a9b0e8557e7f45238f3fef730531f81ff6d
puniyapoojachowdary <poojachowdary2601@gmail.com>
2019-12-19 15:51:33 +0530

    FAVICON ADD

--- public/js/site.js
+++ public/js/site.js
@@ -3,9 +3,9 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/htmls/index.html'))
-var obj={}
-obj.name="Being Zero"
-obj.college="cmrcet"
-obj.reg="17h51a05g5"
+var obj = {}
+obj.name = "Being Zero"
+obj.college = "cmrcet"
+obj.reg = "17h51a05g5"
 app.get('/data', (req, res) => res.json(obj))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
